b8ef3cb93502553c293912e9fab45a3ef9d4ad47
Tyler Ikehara <57115498+tikehara99@users.noreply.github.com>
2020-09-21 16:34:43 -1000

    Update LEDfun.ino

--- Arduino/LEDfun/LEDfun.ino
+++ Arduino/LEDfun/LEDfun.ino
@@ -4,6 +4,7 @@
  * - Tyler Ikehara 2020
 */
 
+//declaring pin numbers
 int redRGBpin = 11;
 int blueRGBpin = 10;
 int greenRGBpin = 9;
@@ -23,6 +24,7 @@ int greenLed = 8;
 int blueLed = 7;
 int redLed = 6;
 
+//initializing variables
 int blueState = 0;
 int redState = 0;
 int greenState = 0;
@@ -32,7 +34,7 @@ int pinVal = 0;
 int colorVal = 0;
 
 int count =0;
-void setup() {
+void setup() { //setting up serial monitor and pinmodes of the LEDs and buttons
   Serial.begin(9600);
   pinMode(redRGBpin, OUTPUT);
   pinMode(blueRGBpin, OUTPUT);
@@ -44,7 +46,7 @@ void setup() {
   pinMode(greenLed, OUTPUT);
   pinMode(blueLed, OUTPUT);
   pinMode(redLed, OUTPUT);
-
+//setting the intial values of the RGB LED to 0, and setting the other LEDs to be off
   analogWrite(redRGBpin, redVal);
   analogWrite(blueRGBpin, blueVal);
   analogWrite(greenRGBpin, greenVal);
@@ -53,15 +55,15 @@ void setup() {
   digitalWrite(redLed, LOW);
 }
 
-void loop() {
+void loop() { //determines which button is being pressed
   blueState = digitalRead(blueButton);
   greenState = digitalRead(greenButton);
   redState = digitalRead(redButton);
   controlState = digitalRead(controlButton);
-  if(blueState == HIGH){
+  if(blueState == HIGH){ //depending on which button is pressed, the same colored LED turns on
     reset();
     digitalWrite(blueLed, HIGH);
-    pinVal = blueRGBpin;
+    pinVal = blueRGBpin; //the pin that we will edit later is changed to the pin of the certain color of the RGB LED
   }
   if(greenState == HIGH){
     reset();
@@ -78,14 +80,14 @@ void loop() {
     digitalWrite(greenLed, HIGH);
     pinVal = greenRGBpin;
   }
-  if(controlState == HIGH){
+  if(controlState == HIGH){ //if the control button is held for a certain time, the program turns everything back off
     if(count <4){
       reset();
     }
     count ++;
     pinVal = 0;
     colorVal= 0;
-    if(count >=4){
+    if(count >=4){ //code to write the RGB LED all to 0
       analogWrite(redRGBpin, colorVal);
       analogWrite(blueRGBpin, colorVal);
       analogWrite(greenRGBpin, colorVal);
@@ -93,23 +95,23 @@ void loop() {
     }
     delay(500);
   }
-  colorVal = LedVal();
-  analogWrite(pinVal, colorVal);
+  colorVal = LedVal(); //determines the value that the potentiometer reads
+  analogWrite(pinVal, colorVal);  //writes the value to the pin that was selected above
   Serial.print("count = ");
   Serial.println(count);
 }
 
-int LedVal(){
-   int potVal = analogRead(A0);
-   int led = map(potVal, 0, 1023, 0, 255);
+int LedVal(){ //function that updates the value of the RGB LED
+   int potVal = analogRead(A0); //reads the value of the potentiometer
+   int led = map(potVal, 0, 1023, 0, 255); //maps the values of the potentiometer to the RGB range
    Serial.print("Pot Val: ");
    Serial.print(potVal);
    Serial.print("\t LED val: ");
    Serial.println(led);
-   return(led);
+   return(led); //returns the remapped value
 }
 
-void reset(){
+void reset(){ //simple code to turn all of the other LEDs off
   digitalWrite(blueLed, LOW);
   digitalWrite(greenLed, LOW);
   digitalWrite(redLed, LOW);
